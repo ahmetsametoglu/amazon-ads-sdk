ada581a1c1fa80a868487b6419264ac0eb9ddc0e
ahmetsametoglu <ahmetsametoglu@gmail.com>
2025-04-19 20:43:22 +0200

    Jest test yapılandırması güncellendi. testMatch ayarı değiştirildi, testPathIgnorePatterns eklendi ve yeni test ayarları (verbose, maxWorkers, reporters, testTimeout) tanımlandı. Ayrıca, kod stilinde bazı iyileştirmeler yapıldı.

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,21 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  roots: ["<rootDir>/src"],
-  testMatch: ["**/__tests__/**/*.ts", "**/?(*.)+(spec|test).ts"],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.test.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/__tests__/test-utils/'],
   transform: {
-    "^.+\\.tsx?$": "ts-jest",
+    '^.+\\.tsx?$': 'ts-jest',
   },
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   watchAll: false,
   watch: false,
   automock: false,
   notify: false,
   watchPlugins: [],
+  verbose: true,
+  maxWorkers: '50%',
+  reporters: ['default'],
+  testTimeout: 30000,
 };
